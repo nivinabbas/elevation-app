0fb82e360f89eade694eacb4969c4366b8298d3b
nivinabbas <nivin.abbas1@gmail.com>
2021-12-14 19:03:28 +0200

    merge conflict

--- dist/students/Controller.js
+++ dist/students/Controller.js
@@ -2,10 +2,6 @@ const dataModel = new StudentModel()
 const renderer = new Renderer()
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> fadi2
 document.ready(async function () {
     await dataModel.getStudents()
     renderer.renderData(dataModel.students)
