288f5792b53a058377a0cd401a6c2377a2ef482b
Mohammad <m7md389@gmail.com>
2021-12-14 18:16:36 +0200

    fix errors

--- server/models/Job.js
+++ server/models/Job.js
@@ -8,12 +8,7 @@ const Job = mongoose.model("Job", new mongoose.Schema({
 }))
 
 
-const getJobsByTitleCompany = (company, title) => Job.find(
-<<<<<<< HEAD
-    {company: company, title: {$regex: title, $options: 'i'}}, 'id title').exec()
-=======
-    {company: {$regex: company, $options: 'i'}, title: {$regex: title, $options: 'i'}}, 'id title').exec()
->>>>>>> 66f524d9c91b3861ce6b27046415022776bbdf9d
+const getJobsByTitleCompany = (company, title) => Job.find({ company: { $regex: company, $options: 'i' }, title: { $regex: title, $options: 'i' } }, 'id title').exec()
 
 
 module.exports = { Job: Job, getJobsByTitleCompany: getJobsByTitleCompany }
