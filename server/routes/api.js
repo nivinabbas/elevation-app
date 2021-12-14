422300d28846dd7518731f958036728d063f8268
Mohammad <m7md389@gmail.com>
2021-12-14 15:34:37 +0200

    whatever

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const RecruitmentProcess = require("./models/RecruitmentProcess")
+const Job = require("./medels/Job.js")
 
+router.get("/jobs", function(req, res) {
+    Job.find({}, function(err, result) {
+        res.send(result)
+    })
+})
 
 module.exports = router
