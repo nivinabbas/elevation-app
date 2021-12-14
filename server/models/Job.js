46db5bcb3bcec1649ca26f7715eaf1d1825acf68
hadifrohar <hadifrohar@gmail.com>
2021-12-14 12:17:50 +0200

    Created job model, updated recruitment process model

---
+++ server/models/Job.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const jobModel = mongoose.model("Job", new mongoose.Schema({
+    company: {type: String, require: true},
+    title: {type: String, require: true},
+    recruitmentProcesses: [{type: mongoose.Types.ObjectId, ref: 'RecruitmentProcess'}]
+})) 
+
+
+module.exports = {jobModel: jobModel}
--- server/models/RecruitmentProcess.js
+++ server/models/RecruitmentProcess.js
@@ -8,8 +8,8 @@ const questionSchema = new mongoose.Schema({
 })
 
 const recruitmentProcessSchema = new mongoose.Schema({
-    jobTitle: {type: String, require: true},
-    jobLink: {type: String, require: true},
+    job: {type: mongoose.Types.ObjectId, ref: 'Job', require: true},
+    jobLink: String,
     recruitmentStatus: {type: Number, require: true},
     appliedStudent: {type: mongoose.Types.ObjectId, ref: "StudentUser", require: true},
     salary: Number,
@@ -18,7 +18,7 @@ const recruitmentProcessSchema = new mongoose.Schema({
 })
 
 
-const recruitmentProcessModel = mongoose.model('Recruitment', recruitmentProcessSchema)
+const recruitmentProcessModel = mongoose.model('RecruitmentProcess', recruitmentProcessSchema)
 
 
 module.exports = {recruitmentProcessModel: recruitmentProcessModel}
