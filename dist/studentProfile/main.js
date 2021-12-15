a9b05b67b5516aa0ae0e085ea48c244c5785604f
Mohammad <m7md389@gmail.com>
2021-12-15 18:24:42 +0200

    add recruitment processes

--- dist/studentProfile/main.js
+++ dist/studentProfile/main.js
@@ -12,6 +12,22 @@ $(document).on("click", "#submit-profile", function() {
     profileModel.editStudentData(student)
 })
 
+$(document).on('click', "#confirmBtn", function() {
+    const inputStage = document.querySelector('select').value
+    const inputName = $('.name').val()
+    const inputSalary = $('.salary').val()
+    const inputQuestions = $('.questions').val()
+    const inputJobLink = $('.jobLink').val()
+
+    let inputs = {
+        name: inputName,
+        salary: inputSalary,
+        questions: inputQuestions,
+        jobLink: inputJobLink,
+        stage: inputStage
+    }
+})
+
 $(document).ready(async function() {
     await profileModel.getStudentrData()
     renderer.renderProfile(profileModel.studentData)
