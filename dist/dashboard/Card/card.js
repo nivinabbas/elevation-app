3519e6f05542113525ee310f1db4c226ba17a190
nivinabbas <nivin.abbas1@gmail.com>
2021-12-15 19:45:22 +0200

    add new route student list

--- dist/dashboard/Card/card.js
+++ dist/dashboard/Card/card.js
@@ -5,10 +5,15 @@ class dataOfCards {
 
   getDataFromDB = async function () {
     this.dataOfCards = await $.get(`http://localhost:8888/dataOfCards/`);
-    console.log('qweqzzzwe');
   };
 
   saveDataOfProcess(process) {
     $.post('/jobs/process', process, function (res) {});
   }
+
+  getDataAboutStudent = async function () {
+    const data = await $.get('studentsList');
+    console.log(data);
+    return data;
+  };
 }
